180481c1f2f8b5078388817d3c798b581fc243d2
lens0021 <lorentz0021@gmail.com>
2026-01-11 23:51:21 +0900

    chore: Allow GitHub Actions to create and approve pull requests

--- .github/tf/main.tf
+++ .github/tf/main.tf
@@ -14,3 +14,52 @@ terraform {
 provider "github" {
   owner = "lens0021"
 }
+
+import {
+  id = "declam"
+  to = github_repository.this
+}
+resource "github_repository" "this" {
+  allow_auto_merge                        = false
+  allow_merge_commit                      = false
+  allow_rebase_merge                      = true
+  allow_squash_merge                      = false
+  allow_update_branch                     = true
+  archived                                = false
+  archive_on_destroy                      = true
+  auto_init                               = false
+  delete_branch_on_merge                  = true
+  description                             = ""
+  has_discussions                         = false
+  has_downloads                           = true
+  has_issues                              = true
+  has_projects                            = false
+  has_wiki                                = false
+  homepage_url                            = ""
+  name                                    = "declam"
+  squash_merge_commit_message             = "COMMIT_MESSAGES"
+  squash_merge_commit_title               = "COMMIT_OR_PR_TITLE"
+  topics                                  = []
+  visibility                              = "public"
+  vulnerability_alerts                    = false
+  web_commit_signoff_required             = false
+
+  security_and_analysis {
+    secret_scanning {
+      status = "enabled"
+    }
+    secret_scanning_push_protection {
+      status = "enabled"
+    }
+  }
+}
+
+import {
+  id = "declam"
+  to = github_workflow_repository_permissions.this
+}
+resource "github_workflow_repository_permissions" "this" {
+  repository = github_repository.this.name
+  default_workflow_permissions = "read"
+  can_approve_pull_request_reviews = true
+}
